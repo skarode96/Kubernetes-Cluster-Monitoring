7dc2d43f6111ce9225271c1f31f3c397cf7e2842
skarode96 <skarode96@gmail.com>
2020-09-03 01:31:46 +0100

    refactor sniffer.py

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd app/ &&
 gunicorn --bind 0.0.0.0:3000 wsgi:app &
 echo "gunicorn started" &
-python sniffer/sniff_packets.py test --filter="tcp port 6379" &
+python sniffer/sniffer.py test --filter="tcp port 6379" &
 echo "sniffing started" &
 tail -f /dev/null
